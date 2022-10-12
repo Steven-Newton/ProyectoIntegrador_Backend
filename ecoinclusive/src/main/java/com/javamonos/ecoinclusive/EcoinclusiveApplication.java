912f549c3acd4acc420aff5cd4186e2a8a5df320
Steven Newton <stevennewton0701@gmail.com>
2022-10-12 15:06:41 -0500

    chore|initialize spring project

---
+++ ecoinclusive/src/main/java/com/javamonos/ecoinclusive/EcoinclusiveApplication.java
@@ -0,0 +1,13 @@
+package com.javamonos.ecoinclusive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoinclusiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoinclusiveApplication.class, args);
+	}
+
+}
---
+++ ecoinclusive/src/test/java/com/javamonos/ecoinclusive/EcoinclusiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamonos.ecoinclusive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoinclusiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
